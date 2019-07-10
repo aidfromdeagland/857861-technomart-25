57f2d829b9dd65bf5fea04e2e3b71006092b9a79
aidfromdeagland <aidfromdeagland@gmail.com>
2019-07-10 20:39:36 +0300

    raw 10072019

---
+++ js/min-js.js
@@ -0,0 +1,64 @@
+
+//cart popup 
+var openCart = document.querySelector(".open-cart");
+var addCart = document.querySelector(".cart-add");
+var cartBlock = document.querySelector(".top-navigation-cart");
+openCart.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  cartBlock.classList.add("make-red");
+  addCart.classList.add("modal-show");
+});
+
+
+var cartClose = document.querySelector(".cart-add-close");
+var cartContinue = document.querySelector(".cart-add-continue");
+cartClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  addCart.classList.remove("modal-show");
+});
+
+cartContinue.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  addCart.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (addCart.classList.contains("modal-show")) {
+      evt.preventDefault();
+      addCart.classList.remove("modal-show");
+    }
+  }
+});
+
+var bookmarks = document.querySelector(".bookmark");
+var bookmarksBlock = document.querySelector(".top-navigation-bookmarks");
+
+bookmarks.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  bookmarksBlock.classList.add("make-red");
+});
+
+//loged-unloged 
+
+var userLogin = document.querySelector(".user-navigation-login");
+var userLoged = document.querySelector(".user-navigation-loged");
+var userUnlog = document.querySelector(".deauthorisation");
+var userLogMenu = document.querySelector(".user-navigation");
+
+userLogin.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  userLoged.classList.add("login-show");
+  userLogMenu.classList.add("login-hide");
+});
+
+userUnlog.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  userLoged.classList.remove("login-show");
+  userLogMenu.classList.remove("login-hide");
+});
+
+//feedback modal
+
+
+
